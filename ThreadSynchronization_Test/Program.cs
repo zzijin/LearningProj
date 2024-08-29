bae782e715e64c5b66142fdde58af368509b6d59
zzijin <zzx1248022549@126.com>
2024-08-30 00:15:37 +0800

    更新线程同步测试

--- ThreadSynchronization_Test/Program.cs
+++ ThreadSynchronization_Test/Program.cs
@@ -6,32 +6,7 @@ namespace ThreadSynchronization_Test
     {
         static void Main(string[] args)
         {
-            //Example.Test();
-
-            bool createdNew;
-            //createdNew=true
-            EventWaitHandle eventWait =new EventWaitHandle(false,EventResetMode.AutoReset, "A",out createdNew);
-            //createdNew=false
-            EventWaitHandle eventWait1 = new EventWaitHandle(false, EventResetMode.ManualReset, "A", out createdNew);
-            Task.Run(() =>
-            {
-                Console.WriteLine("线程启动！");
-                eventWait.WaitOne();
-                Console.WriteLine("线程阻塞解除！#1");
-
-                Console.WriteLine("线程阻塞！");
-                eventWait.WaitOne();
-                Console.WriteLine("线程阻塞解除！#2");
-
-                Console.WriteLine("线程执行完成");
-            });
-            Thread.Sleep(250);
-            Console.WriteLine("回车以释放");
-            Console.ReadLine();
-            eventWait.Set();
-            Console.WriteLine("回车以释放");
-            Console.ReadLine();
-            eventWait.Set();
+            SemaphoreTest.Test();
             //int x = 0;
             //Task.Run(() =>
             //{
@@ -49,63 +24,6 @@ namespace ThreadSynchronization_Test
             //});
 
             Console.ReadLine();
-        }
-        class Example
-        {
-            private static AutoResetEvent event_1 = new AutoResetEvent(true);
-            private static AutoResetEvent event_2 = new AutoResetEvent(false);
-
-            public static void Test()
-            {
-                Console.WriteLine("Press Enter to create three threads and start them.\r\n" +
-                                  "The threads wait on AutoResetEvent #1, which was created\r\n" +
-                                  "in the signaled state, so the first thread is released.\r\n" +
-                                  "This puts AutoResetEvent #1 into the unsignaled state.");
-                Console.ReadLine();
-
-                for (int i = 1; i < 4; i++)
-                {
-                    Thread t = new Thread(ThreadProc);
-                    t.Name = "Thread_" + i;
-                    t.Start();
-                }
-                Thread.Sleep(250);
-
-                for (int i = 0; i < 2; i++)
-                {
-                    Console.WriteLine("Press Enter to release another thread.");
-                    Console.ReadLine();
-                    event_1.Set();
-                    Thread.Sleep(250);
-                }
-
-                Console.WriteLine("\r\nAll threads are now waiting on AutoResetEvent #2.");
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine("Press Enter to release a thread.");
-                    Console.ReadLine();
-                    event_2.Set();
-                    Thread.Sleep(250);
-                }
-
-                // Visual Studio: Uncomment the following line.
-                //Console.Readline();
-            }
-
-            static void ThreadProc()
-            {
-                string name = Thread.CurrentThread.Name;
-
-                Console.WriteLine("{0} waits on AutoResetEvent #1.", name);
-                event_1.WaitOne();
-                Console.WriteLine("{0} is released from AutoResetEvent #1.", name);
-
-                Console.WriteLine("{0} waits on AutoResetEvent #2.", name);
-                event_2.WaitOne();
-                Console.WriteLine("{0} is released from AutoResetEvent #2.", name);
-
-                Console.WriteLine("{0} ends.", name);
-            }
         }
 
         static void VolatileTest()
@@ -163,6 +81,31 @@ namespace ThreadSynchronization_Test
                 Volatile.Write(ref y, 2);
                 var z = y;
             });
+        }
+
+        struct SimpleSpinLock
+        {
+            //设定0代表未使用，1代表使用
+            private int m_ReourceInUse;
+
+            //获取锁
+            public void Enter()
+            {
+                SpinWait spinWait = new SpinWait();
+                //始终循环，使得线程空转
+                while (true)
+                {
+                    //仅当m_ReourceInUse从未使用变为使用时进入
+                    if (Interlocked.Exchange(ref m_ReourceInUse, 1) == 0)
+                        return;
+                }
+            }
+
+            //释放锁
+            public void Leave()
+            {
+                Volatile.Write(ref m_ReourceInUse, 0);
+            }
         }
     }
 }
---
+++ ThreadSynchronization_Test/SemaphoreTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSynchronization_Test
+{
+    internal class SemaphoreTest
+    {
+        public static void Test()
+        {
+            ManualResetEventSlim
+            Semaphore pool = new Semaphore(0, 3);
+            var padding = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var num = i;
+                Task.Run(() =>
+                {
+                    Console.WriteLine($"线程 {num} 等待信号量");
+                    pool.WaitOne();
+
+                    var rs = Interlocked.Add(ref padding, 100);
+
+                    Console.WriteLine($"线程 {num} 获得信号量");
+
+                    Thread.Sleep(1000 + rs);
+
+                    Console.WriteLine($"线程 {num} 释放信号量");
+                    Console.WriteLine($"线程 {num} 释放前的信号量计数为 {pool.Release()}");
+                });
+            }
+
+            Thread.Sleep(500);
+            Console.WriteLine("主线程释放3个信号量");
+            pool.Release(3);
+        }
+    }
+}
