f6168b47b8b8d5b7c0d7117c8b598e5cf9bd7ae4
zzijin <zzx1248022549@126.com>
2025-02-22 01:47:03 +0800

    添加AOT测试

---
+++ AOTTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace AOTTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+
+}
