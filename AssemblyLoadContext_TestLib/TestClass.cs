b6e7451ba30d0a124b20e2020d1ece287b411ce3
zzijin <zzx1248022549@126.com>
2025-02-22 01:47:32 +0800

    补充提交

---
+++ AssemblyLoadContext_TestLib/TestClass.cs
@@ -0,0 +1,35 @@
+﻿using AssemblyLoadContext_TestReferenceLib;
+
+namespace AssemblyLoadContext_TestLib
+{
+    public class TestClass
+    {
+        public static string GetVersion()
+        {
+            //版本号分别为1~3
+            return "3";
+        }
+
+        private int _num;
+        public void SetNum(int num)
+        {
+            _num = num;
+        }
+
+        public int GetNum()
+        {
+            return _num;
+        }
+
+        public void ConsoleNum()
+        {
+            Console.WriteLine($"当前数字为{_num}");
+        }
+
+        public void TestReference()
+        {
+            //程序集依赖于另一个程序集
+            TestReferenceClass.ConsoleReferenceClass();
+        }
+    }
+}
---
+++ AssemblyLoadContext_TestReferenceLib/TestReferenceClass.cs
@@ -0,0 +1,10 @@
+﻿namespace AssemblyLoadContext_TestReferenceLib
+{
+    public static class TestReferenceClass
+    {
+        public static void ConsoleReferenceClass()
+        {
+            Console.WriteLine("这是被引用的程序集！");
+        }
+    }
+}
